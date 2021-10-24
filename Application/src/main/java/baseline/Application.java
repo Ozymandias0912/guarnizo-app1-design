467df5911b5cd41c0422c7844a9f92ffc7654310
Juan <jdavid29a@gmail.com>
2021-10-24 18:47:10 -0400

    added pseudocode and uml diagrams

--- Application/src/main/java/baseline/Application.java
+++ Application/src/main/java/baseline/Application.java
@@ -1,11 +1,56 @@
 /*
- *  UCF COP3330 Summer 2021 Application Assignment 1 Solution
+ *  UCF COP3330 Fall 2021 Application Assignment 1 Solution
  *  Copyright 2021 Juan Guarnizo
  */
 package baseline;
 
 public class Application {
     /*Pseudocode*/
+    //create Item class
+
+    //private Title
+    //private description
+    //private dueDate (YYYY-MM-DD)
+    //private complete
+
+    //public editTitle()
+    //public editDescription()
+    //public editDueDate()
+    //public markComplete
+
+    //end Item class
+
+
+    //create class ToDoList
+
+    //private Title at least 3 characters long
+    //private item[] at least 256 items
+
+    //public addItem()
+    //public removeItem()
+    //public displayItemsInToDoList()
+    //public displayIncompleteItems()
+    //public displayCompleteItems()
+
+    //end class ToDoList
+
+    //create class application
+
+    //private ToDoList[] at least 256 ToDOLists
+
+    //public addToDoList()
+    //public removeToDoList()
+    //public editToDoList()
+    //public saveToDoList()
+        //select the list
+        //specify the file location and name
+        //all selected list should be saved to a single text file
+    //public loadToDoList()
+
+    //end class application
+
+
+
 
 
 }
